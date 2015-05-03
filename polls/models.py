a740c1428f5910bfe4fa2036f3a4b44f6afb87d6
Duy Bach Nguyen <duybach@live.de>
2015-05-03 14:15:39 +0200

    First Commit

---
+++ polls/models.py
@@ -0,0 +1,12 @@
+#from django.db import models
+
+#class todo(models.Model):
+#    todo_text = models.CharField(max_length=200)
+#    todo_perc =
+#    deadline_date = models.DateTimeField('date published')
+
+
+#class Choice(models.Model):
+#    question = models.ForeignKey(Question)
+#    choice_text = models.CharField(max_length=200)
+#    votes = models.IntegerField(default=0)
