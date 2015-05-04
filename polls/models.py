121d477af9fb92d319c1dac9be7491db090228b5
Duy Bach Nguyen <duybach@live.de>
2015-05-04 16:17:24 +0200

    Added Classes. Fix max_length(?)

--- polls/models.py
+++ polls/models.py
@@ -1,12 +1,6 @@
-#from django.db import models
+from django.db import models
 
-#class todo(models.Model):
-#    todo_text = models.CharField(max_length=200)
-#    todo_perc =
-#    deadline_date = models.DateTimeField('date published')
-
-
-#class Choice(models.Model):
-#    question = models.ForeignKey(Question)
-#    choice_text = models.CharField(max_length=200)
-#    votes = models.IntegerField(default=0)
+class todo(models.Model):
+    todo_text = models.CharField('todo', max_length=200)
+    todo_perc = models.IntegerField('perc', default=0, max_length=100)
+    deadline_date = models.DateTimeField('deadline')
