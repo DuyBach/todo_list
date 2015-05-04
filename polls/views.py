9f7f91bc438b24561a79401214d18c0acea6f7ab
Duy Bach Nguyen <duybach@live.de>
2015-05-04 21:08:46 +0200

    Index with template

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+from.models import Todo
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_todo_list = Todo.objects.order_by('-deadline_date')
+    context = {'latest_todo_list': latest_todo_list}
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, todo_id):
+    return HttpResponse("You're looking at question %s." % todo_id)
